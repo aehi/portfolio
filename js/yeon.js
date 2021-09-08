d3af5eb13d247716a647233e19a5e400afd9aea2
aehi <kno1120@naver.com>
2021-09-09 00:38:38 +0900

    first commit

---
+++ js/yeon.js
@@ -0,0 +1,57 @@
+$(function(){
+    // 메뉴 fixed
+    var hd = $('header'),
+        win = $(window);
+
+    win.scroll(function(){
+        win = $(this)
+        win_top = win.scrollTop();
+        
+        if(win_top > 0) {
+            hd.addClass('fixed');
+        }else{
+            hd.removeClass('fixed');
+        };
+
+        var win_h = win.height(),
+            skills = $('.skills_left').offset().top;
+
+        if(win_top >= skills - 400){
+            $('.skills_bar').addClass('ani')
+        }else{ 
+            $('.skills_bar').removeClass('ani')
+        }
+    })
+
+    
+
+    $('.btn_nav').click(function(){
+        $(this).toggleClass('on');
+        $('#navWrap').toggleClass('active');
+    });
+
+    $('.works1 .popup').click(function(){
+        $('.ferrero').show();
+        $('body').addClass('no-scroll');
+    }); 
+    $('.works2 .popup').click(function(){
+        $('.cheongjeongwon').show();
+        $('body').addClass('no-scroll');
+    });    
+    $('.btn_close').click(function(){
+        $('.view_layer').hide();
+        $('body').removeClass('no-scroll');
+    });  
+    
+   
+
+    $('.worksInner .btn_wrap').click(function(){
+        $(this).parent().toggleClass('active');
+
+        if($('.worksInner .btn_wrap').parent().hasClass('active')){
+            $('.btn_more').text('LOAD LESS');
+        }else{
+            $('.btn_more').text('LOAD MORE');
+        }
+    });
+})    
